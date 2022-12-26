9312a0a43593a97324a878b4140d9212b2241807
okasyun <shunshun1207shun@keio.jp>
2022-12-26 22:42:35 +0900

    カクテルについてのアンケート選択肢を追加

---
+++ resources/js/data/selectOptionsData.js
@@ -0,0 +1,52 @@
+export const baseOptions = [
+    { id: 1, value: 1, label: "ジン" },
+    { id: 2, value: 2, label: "ウォッカ" },
+    { id: 3, value: 3, label: "テキーラ" },
+    { id: 4, value: 4, label: "ラム" },
+    { id: 5, value: 5, label: "ウィスキー" },
+    { id: 6, value: 6, label: "ブランデー" },
+    { id: 7, value: 7, label: "リキュール" },
+    { id: 8, value: 8, label: "ワイン" },
+    { id: 9, value: 9, label: "ビール" },
+    { id: 10, value: 10, label: "日本酒" },
+    { id: 11, value: 0, label: "ノンアルコール" },
+];
+
+export const techniqueOptions = [
+    { id: 1, value: 1, label: "ビルド" },
+    { id: 2, value: 2, label: "ステア" },
+    { id: 3, value: 3, label: "シェイク" },
+];
+
+export const tasteOptions = [
+    { id: 1, value: 1, label: "甘口" },
+    { id: 2, value: 2, label: "中甘口" },
+    { id: 3, value: 3, label: "中口" },
+    { id: 4, value: 4, label: "中辛口" },
+    { id: 5, value: 5, label: "辛口" },
+];
+
+export const styleOptions = [
+    { id: 1, value: 1, label: "ショート" },
+    { id: 2, value: 2, label: "ロング" },
+];
+
+export const topOptions = [
+    { id: 1, value: 1, label: "食前" },
+    { id: 2, value: 2, label: "食後" },
+    { id: 3, value: 3, label: "オール" },
+];
+
+export const glassOptions = [
+    { id: 1, value: 1, label: "カクテルグラス" },
+    { id: 2, value: 2, label: "オールドファッションドグラス" },
+    { id: 3, value: 3, label: "コリンズグラス" },
+    { id: 4, value: 4, label: "タンブラー" },
+    { id: 5, value: 5, label: "ワイングラス" },
+    { id: 6, value: 6, label: "シャンパングラス" },
+    { id: 7, value: 7, label: "ホットグラス" },
+    { id: 8, value: 8, label: "ゴブレット" },
+    { id: 9, value: 9, label: "リキュールグラス" },
+    { id: 10, value: 10, label: "サワーグラス" },
+    { id: 11, value: 11, label: "ビルスナーグラス" },
+];
